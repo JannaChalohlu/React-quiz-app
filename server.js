c69119f7f3ae1a740eef2932d28bec7a4dbfbd80
Zhanna <zhanna.chalohlu@dci-student.org>
2024-09-24 11:36:04 +0200

    work with server.js

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const app = express()
 app.use(
     cors({
       credentials: true,
-      origin: ["http://localhost:5174"],
+      origin: ["http://localhost:5174", "https://react-quiz-app-cbaq.onrender.com"],
     })
   );
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
  
-  app.use(express.static(path.join(__dirname, "client/dist", "index.html")));
+  app.use(express.static(path.join(__dirname, "client/dist")));
 app.use(express.json())
 
 app.use('/question', questionRoute)
